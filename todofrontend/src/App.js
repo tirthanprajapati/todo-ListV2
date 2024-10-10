43c9f4307d8c3730becec96e6e1fd0adc4d8b797
Tirthan Prajapati <tirthanbhuvan@gmail.com>
2024-10-10 20:06:01 +0530

    Initial Commit

---
+++ todofrontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="h-screen bg-gray-100 flex justify-center items-center">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Todo List App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
